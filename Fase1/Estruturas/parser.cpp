0e5d73dca07328ce57c779cc7da13d178b1e4b58
José Pedro Ferreira <zepedroferreira11@gmail.com>
2018-03-06 21:31:04 +0000

    Replace parser.cpp

--- Fase1/Estruturas/parser.cpp
+++ Fase1/Estruturas/parser.cpp
@@ -1,11 +1,49 @@
 #include "tinyxml2.h"
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
 
 using namespace tinyxml2;
+using namespace std;
 
 #ifndef XMLCheckResult
 	#define XMLCheckResult(a_eResult) if (a_eResult != XML_SUCCESS) { printf("Error: %i\n", a_eResult); return a_eResult; }
 #endif
+
+void extraiFicheiro(string filename){
+
+	ifstream inputFileStream(filename);
+
+	int count;
+    inputFileStream>>count;
+
+    cout << count << endl;
+
+	string line;
+    getline(inputFileStream, line);
+
+    cout << line << endl;
+
+    for(int i=0;i<count;i++){
+    	getline(inputFileStream, line);
+    	cout << line << endl;
+    	stringstream ss(line);
+    	vector<int> numbers;
+
+    	for(int k = 0; k < 3; k++) {
+    		string aux;
+    		ss >> aux; 
+    		float j = stof(aux);
+        	numbers.push_back(j);
+        	cout << aux << " ";
+        	cout << "" << endl;
+    	}
+    }
+
+    // cout << line << endl;
+}
 
 
 tinyxml2::XMLError parseFile(){
@@ -31,6 +69,11 @@ tinyxml2::XMLError parseFile(){
 
 
 	std::cout << cenas << std::endl;
+
+	string fich(cenas, strlen(cenas));
+
+
+	extraiFicheiro(fich);
 
 }
 
